a5a84b8b6e147c68f90ec92c7aef9b541564ae38
LuyuZhang00 <barry_zly@163.com>
2023-07-11 16:33:19 +0800

    0711 取消订单模块完成

---
+++ service/service-order/src/main/java/com/barry/order/receiver/OrderReceiver.java
@@ -0,0 +1,5 @@
+package com.barry.order.receiver;/**
+* @Author : Luyu Zhang
+* @create 2023-07-11 16:28
+*/public class OrderReceiver {
+}
---
+++ service/service-task/src/main/java/com/barry/task/scheduled/ScheduledTask.java
@@ -0,0 +1,5 @@
+package com.barry.task.scheduled;/**
+* @Author : Luyu Zhang
+* @create 2023-07-11 16:21
+*/public class ScheduledTask {
+}
